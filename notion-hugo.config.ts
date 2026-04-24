ada13a279aba6a49ccfd740b46bb04ce03b37a2a
GitHub Action <action@github.com>
2026-04-25 00:06:37 +0200

    Fix Notion config: set manual=true, update base_url for GitHub Pages

--- notion-hugo.config.ts
+++ notion-hugo.config.ts
@@ -1,25 +1,17 @@
 import { UserConfig } from "./src/config"
 
 const userConfig: UserConfig = {
-    base_url: "https://notion-hugo.pages.dev/",
+    base_url: "https://ekston.github.io/Notion-Hugo/",
     mount: {
-        manual: false,
+        manual: true,
         page_url: 'https://google.com',
         pages: [
-            // {
-            //     page_id: '<page_id>',
-            //     target_folder: 'path/relative/to/content/folder'
-            // }
             {
                 page_id: '45eb121158b9489480ec000fd25c812b',
                 target_folder: '.'
             }
         ],
         databases: [
-            // {
-            //     database_id: '<database_id>',
-            //     target_folder: 'path/relative/to/content/folder'
-            // }
             {
                 database_id: 'b7b1816c05ec464391c8c111fa242985',
                 target_folder: '.'
