67d610d2a31152feac3b760dca81a7e4bc9bf900
Phillip Büchler <4642322+ekston@users.noreply.github.com>
2026-04-24 16:07:55 +0200

    Update notion-hugo.config.ts

--- notion-hugo.config.ts
+++ notion-hugo.config.ts
@@ -4,7 +4,7 @@ const userConfig: UserConfig = {
     base_url: "https://notion-hugo.pages.dev",
     mount: {
         manual: false,
-        page_url: 'https://pcloud.notion.site/Notion-DoIt-04bcc51cfe4c49938229c35e4f0a6fb6',
+        page_url: 'https://www.notion.so/Notion-Hugo-2b35484b5bd783df8cd8814d8077af53?source=copy_link',
         pages: [
             // {
             //     page_id: '<page_id>',
